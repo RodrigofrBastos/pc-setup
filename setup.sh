971510514a5b1efb777072e52bdf5d99823716b9
Rodrigo <99812296+RodrigofrBastos@users.noreply.github.com>
2023-08-29 14:46:15 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,6 +20,9 @@ sudo apt update
 sudo apt install code
 code --version
 
+# c++ for vscode
+sudo apt install g++
+sudo apt install gcc
 
 # Install clickup
 sudo apt install snapd
@@ -54,3 +57,22 @@ sudo apt install ros-dev-tools
 
 
 # Install docker
+## remove any older versions of docker
+
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo docker run hello-world
